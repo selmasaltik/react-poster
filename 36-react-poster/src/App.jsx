6d55c75bae11a136d7016bf0a8c8eda37a829f8d
selmasaltik <selmasaltiik@gmail.com>
2025-05-16 18:26:26 +0300

    Passing Data to Components with Props

--- 36-react-poster/src/App.jsx
+++ 36-react-poster/src/App.jsx
@@ -3,10 +3,8 @@ import Post from './components/Post';
 function App() {
   return (
     <main>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post author="Maximilian" body="React.js is awesome!" />
+      <Post author="Alexander" body="Check out the full course!" />
     </main>
   );
 }
